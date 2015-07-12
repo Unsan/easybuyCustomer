50628de1a60c4b1429db2a0cfd3b472dcd918d4c
Unsan <ylzhang0806@gmail.com>
2015-07-12 12:59:03 +0800

    Initial Commit

---
+++ easybuyCustomer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  easybuyCustomer
+//
+//  Created by Unsan on 12/7/15.
+//  Copyright (c) 2015 NTU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ easybuyCustomerTests/easybuyCustomerTests.swift
@@ -0,0 +1,36 @@
+//
+//  easybuyCustomerTests.swift
+//  easybuyCustomerTests
+//
+//  Created by Unsan on 12/7/15.
+//  Copyright (c) 2015 NTU. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class easybuyCustomerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
